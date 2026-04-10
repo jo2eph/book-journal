822b1e63cbd06947b1f5295c29261c2ebd3a952b
jo2eph <junhyuk.x.lee@gmail.com>
2026-04-10 00:18:30 -0700

    Fix issue in script.js; add fix styles for book cards

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
 const scrollDown = document.getElementById("scroll-down");
 
 scrollDown.addEventListener('click', () => {
-    const section1 = document.getElementById("section-1");
-    section1.scrollIntoView(
+    const books = document.getElementById("books-container");
+    books.scrollIntoView(
         {behavior: "smooth"}
     )
 })
